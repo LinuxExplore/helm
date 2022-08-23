861a26ec3dca6e281a5823189d2c27d61d63a615
rahulpanwar <panwar.rahul@gmail.com>
2022-08-24 00:02:19 +0900

    Create helm-create.sh

---
+++ scripts/helm-create.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+helm create le-app
+
+#############
+# tree le-app
+
+<< DIRECTORY_LE
+le-app/
+|-- Chart.yaml
+|-- charts
+|-- templates
+|   |-- NOTES.txt
+|   |-- _helpers.tpl
+|   |-- deployment.yaml
+|   |-- hpa.yaml
+|   |-- ingress.yaml
+|   |-- service.yaml
+|   |-- serviceaccount.yaml
+|   `-- tests
+|       `-- test-connection.yaml
+`-- values.yaml
+
+3 directories, 10 files
+DIRECTORY_LE
+
+#############
+# cat Chart.yaml 
+
+<< CAT_CHART
+apiVersion: v2
+name: le-app
+description: A Helm chart for Kubernetes
+
+# A chart can be either an 'application' or a 'library' chart.
+#
+# Application charts are a collection of templates that can be packaged into versioned archives
+# to be deployed.
+#
+# Library charts provide useful utilities or functions for the chart developer. They're included as
+# a dependency of application charts to inject those utilities and functions into the rendering
+# pipeline. Library charts do not define any templates and therefore cannot be deployed.
+type: application
+
+# This is the chart version. This version number should be incremented each time you make changes
+# to the chart and its templates, including the app version.
+# Versions are expected to follow Semantic Versioning (https://semver.org/)
+version: 0.1.0
+
+# This is the version number of the application being deployed. This version number should be
+# incremented each time you make changes to the application. Versions are not expected to
+# follow Semantic Versioning. They should reflect the version the application is using.
+# It is recommended to use it with quotes.
+appVersion: "1.16.0"
+CAT_CHART
+
+# By default helm will create package with nginx application
+# To deploy le-app using helm chart, use below command:
+# helm install le-app le-app/
